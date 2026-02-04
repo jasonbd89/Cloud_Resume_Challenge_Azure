6764e8d3316071e6509a2e3329aea52cd4e60303
jasonbd89 <61833788+jasonbd89@users.noreply.github.com>
2026-02-04 11:56:12 +0100

    add Terraform provider and resource group configuration

---
+++ terraform/provider.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0" 
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "state-file-tf"
+    storage_account_name = "statefileazlukas" # The one you made in the GUI
+    container_name       = "tf-state"
+    key                  = "prod.terraform.tf-state"
+    use_azuread_auth     = true # Tells Terraform to use your login/identity
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
