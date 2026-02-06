57c644728d0c76964b143f1d331153e86e3f8ef0
jasonbd89 <61833788+jasonbd89@users.noreply.github.com>
2026-02-06 22:38:33 +0100

    fix: update function app configuration and restore function API URL output

--- terraform/function.tf
+++ terraform/function.tf
@@ -1,55 +1,36 @@
 resource "azurerm_service_plan" "func_plan" {
-  name                = "sp-resume-challenge"
+  name                = "asp-resume-challenge"
   resource_group_name = azurerm_resource_group.resume-challenge.name
-  location            = "westeurope"
+  location            = azurerm_resource_group.resume-challenge.location
   os_type             = "Linux"
-  sku_name            = "Y1"
+  sku_name            = "Y1" # Consumption plan
 }
 
 resource "azurerm_linux_function_app" "visitor_counter" {
-  name                = "func-visitor-counter-resume-022025"
+  name                = "func-resume-challenge-022025"
   resource_group_name = azurerm_resource_group.resume-challenge.name
-  location            = "westeurope"
+  location            = azurerm_resource_group.resume-challenge.location
 
   storage_account_name       = azurerm_storage_account.func_storage.name
   storage_account_access_key = azurerm_storage_account.func_storage.primary_access_key
   service_plan_id            = azurerm_service_plan.func_plan.id
 
-  identity {
-    type = "SystemAssigned"
-  }
-
   site_config {
     application_stack {
-      python_version = "3.10"
+      python_version = "3.11"
     }
     cors {
-      allowed_origins = [trim(azurerm_storage_account.resume-challenge.primary_web_endpoint, "/")]
+      # Allow the static website to call this function. 
+      # trimsuffix removes the trailing slash which CORS often dislikes.
+      allowed_origins = [trimsuffix(azurerm_storage_account.resume-challenge.primary_web_endpoint, "/")]
+      support_credentials = true
     }
   }
 
   app_settings = {
-    "COSMOS_ENDPOINT"              = azurerm_cosmosdb_account.resume-challenge-ac.endpoint
-    "SCM_DO_BUILD_DURING_DEPLOYMENT" = "false"
-    "ENABLE_ORYX_BUILD"            = "false"
-    "FUNCTIONS_WORKER_RUNTIME"     = "python"
-    "AzureWebJobsFeatureFlags"     = "EnableWorkerIndexing"
-    "WEBSITE_RUN_FROM_PACKAGE" = "https://placeholder.zip"
-
-  }
-
-  lifecycle {
-    ignore_changes = [
-      app_settings["WEBSITE_RUN_FROM_PACKAGE"],
-      app_settings["WEBSITE_ENABLE_SYNC_UPDATE_SITE"]
-    ]
+    "AzureResumeConnectionString" = azurerm_cosmosdb_account.resume-challenge-ac.primary_sql_connection_string
+    "FUNCTIONS_WORKER_RUNTIME"    = "python"
+    "CosmosDbDatabaseName"        = azurerm_cosmosdb_sql_database.resume-challenge-db.name
+    "CosmosDbContainerName"       = azurerm_cosmosdb_sql_container.visitor-counter.name
   }
 }
-
-resource "azurerm_cosmosdb_sql_role_assignment" "func_to_cosmos" {
-    resource_group_name = azurerm_resource_group.resume-challenge.name   
-    account_name        = azurerm_cosmosdb_account.resume-challenge-ac.name
-    role_definition_id  = "${azurerm_cosmosdb_account.resume-challenge-ac.id}/sqlRoleDefinitions/00000000-0000-0000-0000-000000000002"
-    principal_id        = azurerm_linux_function_app.visitor_counter.identity[0].principal_id
-    scope               = azurerm_cosmosdb_account.resume-challenge-ac.id
- }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,6 +3,6 @@ output "primary_web_endpoint" {
   value       = azurerm_storage_account.resume-challenge.primary_web_endpoint
 }
 
-#output "function_api_url" {
-#  value = "https://${azurerm_linux_function_app.visitor_counter.default_hostname}/api/visitors"
-#}
+output "function_api_url" {
+  value = "https://${azurerm_linux_function_app.visitor_counter.default_hostname}/api/visitors"
+}
